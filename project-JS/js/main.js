3afccc4f91acffaa3241c1f26356dd19c93a802e
Ray Hung <hungk901@newschool.edu>
2015-09-28 16:39:36 -0400

    Add Moment.js

--- project-JS/js/main.js
+++ project-JS/js/main.js
@@ -4,6 +4,51 @@ var state = '';
 var country = '';
 var APIKey = '6f206707a93aceba';
 
+var updateTime = function(globalTimeUTC){
+    // Get Local UTC.
+    var thisUTC = new Date();
+    thisUTC = (thisUTC.getTimezoneOffset())/60;
+
+    var globalTimeUTC = Number(globalTimeUTC);
+    console.log(globalTimeUTC);
+
+    var thisDate = moment().utcOffset(globalTimeUTC).format('ddd, MMM DD, YYYY')
+
+    var thisTime = moment().utcOffset(globalTimeUTC).format('HH:mm:ss');
+
+    // Get Local UTC.
+    // var thisUTC = new Date();
+    // thisUTC = (thisUTC.getTimezoneOffset())/60;
+
+    // var showTime = UTC_0 + ':' + thisMinute + ':' + thisSecond;
+
+    console.log(thisDate);
+    console.log(thisTime);
+
+    // setTimeout(updateTime, 1000);
+
+    // console.log(UTC_0);
+    // console.log(showTime);
+
+    $('.time').text(thisTime);
+    $('.date').text(thisDate);
+}
+
+var loadTime = function(response){
+    if (response.response.error) {
+        // alert(response.response.error);
+        alert('Please check your input!');
+        return;
+    };
+
+    // Transfer the "Hour String" into "Number".
+    var globalTimeUTC = Number((response.current_observation.local_tz_offset).slice(0, 3).replace(/0/g, ''));
+
+    // console.log(globalTimeUTC);
+
+    updateTime(globalTimeUTC);
+}
+
 var loadWeather = function(response){
     if (response.response.error) {
         // alert(response.response.error);
@@ -12,42 +57,20 @@ var loadWeather = function(response){
     };
     // console.log("response=" +JSON.stringify(response));
 
+    var globalCity = response.current_observation.display_location.city;
+    var globalState = response.current_observation.display_location.state;
+    var globalCountry = response.current_observation.display_location.country_iso3166;
+    var globalTemp = response.current_observation.temp_f;
+    var globalWeather = response.current_observation.weather;
 
-    var thisCity = response.current_observation.display_location.city;
-    var thisState = response.current_observation.display_location.state;
-    var thisCountry = response.current_observation.display_location.country_iso3166;
-    var thisTemp = response.current_observation.temp_f;
-    var thisWeather = response.current_observation.weather;
-    var thisTime = response.current_observation.local_time_rfc822;
-
-    var thisTimeArray = [];
-    thisTimeArray = thisTime.split(" ", 5);
-
-    // for (var i = 0; i < thisTimeArray.length; i++) {
-    //     console.log(thisTimeArray[i]);
-    // };
-
-    var showDate = thisTimeArray[0] + ' ' + thisTimeArray[2] + ' ' + thisTimeArray[1] + ', ' + thisTimeArray[3];
-    var showTime = thisTimeArray[4];
-
-     // console.log(showDate);
-     // console.log(thisCity);
-     // console.log(thisTemp);
-     // console.log(thisWeather);
-     // console.log(thisTime);
-
-     $('.date').text(showDate);
-     $('.time').text(showTime);
-
-     if (thisState != '') {
-        $('.currentCity').val(thisCity + ', ' + thisState + ', ' + thisCountry);
-     }
-     else {
-        $('.currentCity').val(thisCity + ', ' + thisCountry);
-     };
-
-     $('.temperature').text(thisTemp);
-     $('.weather').text(thisWeather);
+    if (globalState != '') {
+        $('.currentCity').val(globalCity + ', ' + globalState + ', ' + globalCountry);
+    }
+    else {
+        $('.currentCity').val(globalCity + ', ' + globalCountry);
+    };
+    // $('.temperature').text(globalTemp);
+    // $('.weather').text(globalWeather);
 };
 
 var getWeather = function(){
@@ -58,6 +81,7 @@ var getWeather = function(){
         dataType : 'jsonp',
         success : function(response){
             loadWeather(response);
+            loadTime(response);
         }
     });
 };
@@ -74,13 +98,11 @@ var getPhoto = function(){
     function(data) {
         $.each(data.items, function(i, item) {
             var flickrPhoto = item.media.m;
-            flickrPhoto = flickrPhoto.split("_m.jpg", 1) + "_h.jpg";    // Restructuring the URL to get HQ photo.
-            // console.log(flickrPhoto);
+            flickrPhoto = flickrPhoto.split("_m.jpg", 1) + "_b.jpg";    // Restructuring the URL to get HQ photo.
 
             var imageURL = ' url("' + flickrPhoto + '") ';
             $('.images').css("background-image", imageURL);
 
-            // console.log(imageURL);
             if (i === 0) {
                 return false;
             };
@@ -89,9 +111,8 @@ var getPhoto = function(){
 };
 
 var setLocation = function(){
-
     var locationArray = [];
-    locationArray = $('.currentCity').val().split(",")
+    locationArray = $('.currentCity').val().split(", ")
 
     city = locationArray[0];
     country = locationArray[1];
@@ -103,8 +124,6 @@ var setLocation = function(){
         alert('You need to input a city.');
     };
     // state = $('.currentState').val();
-    // console.log('We want the weather date for ' + city + ' / ' + state);
-    // console.log('We want the weather date for ' + city);
 
     getWeather();
     getPhoto();
@@ -119,4 +138,5 @@ var init = function(){
 
 $(document).ready(function(){
     init();
+    setTimeout(updateTime, 1000);
 });
